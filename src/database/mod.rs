f000dc55591d5862a26e4b49765a6b5be816acb3
Quaternion <quat3rnion@proton.me>
2023-05-23 18:36:29 -0400

    mod imports and Queryer trait

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,12 +1,25 @@
-use crate::errors::Error;
+mod application;
+mod config;
+mod user;
 
-pub async fn establish_connection() -> Result<sqlx::AnyPool, Error> {
+pub use application::ApplicationService;
+pub use config::ConfigService;
+pub use user::UserService;
+
+use polyphony_types::errors::Error;
+use sqlx::{Executor, MySqlPool};
+
+pub trait Queryer<'c>: Executor<'c, Database = sqlx::MySql> {}
+
+impl<'c> Queryer<'c> for &MySqlPool {}
+
+pub async fn establish_connection() -> Result<sqlx::MySqlPool, Error> {
     let db_url = std::env::var("DATABASE_URL").unwrap_or({
-        log::warn!(target: "spacebar::db", "You did not specify `DATABASE_URL` environment variable, defaulting to in-memory SQLlite.");
+        log::warn!(target: "symfonia::db", "You did not specify `DATABASE_URL` environment variable, defaulting to in-memory SQLlite.");
         "sqlite::memory:".to_string()
     });
 
-    let pool = sqlx::AnyPool::connect(&db_url).await?;
+    let pool = sqlx::MySqlPool::connect(&db_url).await?;
 
     Ok(pool)
 }
