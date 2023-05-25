923c99c8403d85112a90478b69a7b6059d2798b3
Zertex <me@zertex.dev>
2023-05-25 04:30:15 -0400

    Use unwrap_or_else to prevent the warning to be run when not needed

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -14,7 +14,7 @@ pub trait Queryer<'c>: Executor<'c, Database = sqlx::MySql> {}
 impl<'c> Queryer<'c> for &MySqlPool {}
 
 pub async fn establish_connection() -> Result<sqlx::MySqlPool, Error> {
-    let db_url = std::env::var("DATABASE_URL").unwrap_or({
+    let db_url = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
         log::warn!(target: "symfonia::db", "You did not specify `DATABASE_URL` environment variable, defaulting to in-memory SQLlite.");
         "sqlite::memory:".to_string()
     });
