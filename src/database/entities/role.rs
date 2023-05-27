b65c7c337a8fdf4d2198edc3af626e0cc5a8275b
Quaternion <quat3rnion@proton.me>
2023-05-27 13:13:16 -0400

    Add base create methods for guild,channel,role

--- src/database/entities/role.rs
+++ src/database/entities/role.rs
@@ -1,3 +1,4 @@
+use crate::{database::Queryer, errors::Error};
 use chorus::types::Snowflake;
 use serde::{Deserialize, Serialize};
 use std::ops::{Deref, DerefMut};
@@ -20,5 +21,60 @@ impl Deref for Role {
 impl DerefMut for Role {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.inner
+    }
+}
+
+impl Role {
+    pub async fn create<'c, C: Queryer<'c>>(
+        db: C,
+        id: Option<Snowflake>,
+        guild_id: &Snowflake,
+        name: &str,
+        color: f64,
+        hoist: bool,
+        managed: bool,
+        mentionable: bool,
+        permissions: &str,
+        position: u16,
+        icon: Option<String>,
+        unicode_emoji: Option<String>,
+    ) -> Result<Self, Error> {
+        let role = Self {
+            inner: chorus::types::RoleObject {
+                id: if let Some(sf) = id {
+                    sf
+                } else {
+                    Snowflake::default()
+                },
+                color,
+                hoist,
+                managed,
+                mentionable,
+                name: name.to_string(),
+                permissions: permissions.to_string(),
+                position,
+                icon: icon.to_owned(),
+                unicode_emoji: unicode_emoji.to_owned(),
+                ..Default::default()
+            },
+            guild_id: guild_id.to_owned(),
+        };
+
+        sqlx::query("INSERT INTO roles (id, guild_id, name, color, hoist, managed, mentionable, permissions, position, icon, unicode_emoji) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+            .bind(&role.id)
+            .bind(&role.guild_id)
+            .bind(&role.name)
+            .bind(role.color)
+            .bind(role.hoist)
+            .bind(role.managed)
+            .bind(role.mentionable)
+            .bind(&role.permissions)
+            .bind(role.position)
+            .bind(&role.icon)
+            .bind(&role.unicode_emoji)
+            .execute(db)
+            .await?;
+
+        Ok(role)
     }
 }
