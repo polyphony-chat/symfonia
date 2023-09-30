4a99f2a30f34c9c7871096f4141b75ab8ce7bca0
Quaternion <quat3rnion@proton.me>
2023-09-29 20:56:08 -0400

    Add token checking logic

---
+++ src/util/token.rs
@@ -0,0 +1,26 @@
+use crate::database::entities::User;
+use crate::errors::{Error, UserError};
+use chorus::types::jwt::Claims;
+use jsonwebtoken::TokenData;
+use sqlx::MySqlPool;
+
+pub async fn check_token(db: &MySqlPool, token: &str, jwt_secret: &str) -> Result<Claims, Error> {
+    let decoding_key = jsonwebtoken::DecodingKey::from_base64_secret(jwt_secret).unwrap();
+    let mut validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::HS256);
+    validation.insecure_disable_signature_validation(); // TODO: Remove
+    let token: TokenData<Claims> = jsonwebtoken::decode(token, &decoding_key, &validation).unwrap();
+
+    let user = User::get_by_id(db, token.claims.id)
+        .await
+        .unwrap()
+        .ok_or(Error::User(UserError::InvalidUser))?;
+
+    if chrono::DateTime::from_timestamp(token.claims.iat, 0).unwrap() < user.data.valid_tokens_since
+    {
+        return Err(Error::User(UserError::InvalidToken));
+    }
+
+    // TODO: Check if user is banned or disabled
+
+    Ok(token.claims)
+}
