36f3995aafdd269af8034125f351880ae993fd67
Quaternion <quat3rnion@proton.me>
2023-09-29 20:58:01 -0400

    Add register, @me routes.  finish login route

--- src/api/routes/auth/mod.rs
+++ src/api/routes/auth/mod.rs
@@ -1,3 +1,12 @@
 mod login;
+mod register;
 
 pub use login::*;
+use poem::{post, Route};
+pub use register::*;
+
+pub fn setup_routes() -> Route {
+    Route::new()
+        .at("/login", post(login))
+        .at("/register", post(register))
+}
---
+++ src/api/routes/auth/register.rs
@@ -0,0 +1,55 @@
+use crate::database::entities::{Config, User};
+use crate::util;
+use chorus::types::jwt::generate_token;
+use chorus::types::{APIError, AuthError, RegisterSchema};
+use poem::web::{Data, Json};
+use poem::{handler, IntoResponse, Request};
+use serde_json::json;
+
+#[handler]
+pub async fn register(
+    Data(db): Data<&sqlx::MySqlPool>,
+    Data(cfg): Data<&Config>,
+    Json(payload): Json<RegisterSchema>,
+    req: &Request,
+) -> Result<impl IntoResponse, APIError> {
+    // TODO: Check reg token
+
+    if !payload.consent {
+        // TODO: Fail consent
+    }
+
+    // TODO: Check registration disabled
+    if let Some(exists) =
+        User::get_user_by_email_or_phone(db, payload.email.as_ref().unwrap().as_str(), "")
+            .await
+            .expect("Failed to check if user exists")
+    {
+        return Err(APIError::Auth(AuthError::InvalidLogin)); // TODO: Change error
+    }
+
+    // TODO: All field checks
+
+    let user = User::create(
+        db,
+        cfg,
+        &payload.username,
+        payload.password,
+        payload.email,
+        payload.fingerprint,
+        payload.date_of_birth,
+        false,
+    )
+    .await
+    .expect("Failed to create user");
+
+    // TODO: Invite
+
+    let token = generate_token(
+        user.id,
+        user.email.clone().unwrap(),
+        &cfg.security.jwt_secret,
+    );
+
+    Ok(Json(json!({"token": token})))
+}
