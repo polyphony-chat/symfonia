8459b19f76b05fbf67f2ef14276fcdb01b4c72c2
Quaternion <quat3rnion@proton.me>
2023-10-06 19:40:30 -0400

    Add GuildError handling and refactor response codes
    This commit introduces GuildError, an additional error type which includes errors specific to guild operations. It also refactors Error and UserError to provide more meaningful HTTP response status codes based upon the specific error type encountered. The improved status code handling provides more clarity for client-side error handling and debugging.

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,16 @@
+use chorus::types::{APIError, AuthError};
+use poem::error::ResponseError;
+use poem::http::StatusCode;
+use poem::Response;
+use std::error::Error as StdError;
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(transparent)]
     User(#[from] UserError),
+
+    #[error(transparent)]
+    Guild(#[from] GuildError),
 
     #[error("SQLX error: {0}")]
     SQLX(#[from] sqlx::Error),
@@ -26,4 +35,53 @@ pub enum UserError {
     InvalidUser,
     #[error("INVALID_TOKEN")]
     InvalidToken,
+    #[error("ALREADY_EXISTS")]
+    AlreadyExists,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum GuildError {
+    #[error("GUILD_NOT_FOUND")]
+    InvalidGuild,
+    #[error("MEMBER_NOT_FOUND")]
+    MemberNotFound,
+    #[error("ALREADY_IN_GUILD")]
+    AlreadyInGuild,
+}
+
+impl ResponseError for Error {
+    fn status(&self) -> StatusCode {
+        match self {
+            Error::User(err) => match err {
+                UserError::InvalidEmail => StatusCode::BAD_REQUEST,
+                UserError::InvalidDiscriminator => StatusCode::BAD_REQUEST,
+                UserError::InvalidUser => StatusCode::NOT_FOUND,
+                UserError::InvalidToken => StatusCode::UNAUTHORIZED,
+                UserError::AlreadyExists => StatusCode::BAD_REQUEST,
+            },
+            Error::Guild(err) => match err {
+                GuildError::InvalidGuild => StatusCode::NOT_FOUND,
+                GuildError::MemberNotFound => StatusCode::NOT_FOUND,
+                GuildError::AlreadyInGuild => StatusCode::BAD_REQUEST,
+            },
+            Error::SQLX(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::Serde(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::IO(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::Chorus(err) => match err {
+                APIError::Auth(auth_err) => match auth_err {
+                    AuthError::InvalidLogin => StatusCode::UNAUTHORIZED,
+                    AuthError::InvalidCaptcha => StatusCode::BAD_REQUEST,
+                },
+            },
+        }
+    }
+
+    fn as_response(&self) -> Response
+    where
+        Self: StdError + Send + Sync + 'static,
+    {
+        Response::builder()
+            .status(self.status())
+            .body(self.to_string())
+    }
 }
