979c0a411d5b6286cb8417f237c9e5fdf7472b15
zertex <me@zertex.dev>
2023-10-06 19:47:45 -0400

    Add 'guilds' and 'healthz' routes and improve error handling in API module

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,13 +2,15 @@ mod middleware;
 mod routes;
 
 use poem::middleware::{NormalizePath, TrailingSlash};
+use poem::web::Json;
 use poem::{
     listener::{Listener, TcpListener},
-    post, EndpointExt, Route, Server,
+    post, EndpointExt, IntoResponse, Route, Server,
 };
+use serde_json::json;
 
 use crate::api::middleware::authentication::AuthenticationMiddleware;
-use crate::api::routes::{auth, users};
+use crate::api::routes::{auth, guilds, users};
 use crate::{database, database::entities::Config, errors::Error};
 
 pub async fn start_api() -> Result<(), Error> {
@@ -35,16 +37,29 @@ pub async fn start_api() -> Result<(), Error> {
             "/users",
             users::setup_routes().with(AuthenticationMiddleware),
         )
+        .nest(
+            "/guilds",
+            guilds::setup_routes().with(AuthenticationMiddleware),
+        )
         .nest("/policies", routes::policies::setup_routes())
+        .nest("/-", routes::health::setup_routes())
         .data(db)
         .data(config)
         .with(NormalizePath::new(TrailingSlash::Trim));
 
-    let v9_api = Route::new().nest("/api/v9", routes);
+    let v9_api = Route::new()
+        .nest("/api/v9", routes)
+        .catch_all_error(custom_error);
 
     let bind = std::env::var("API_BIND").unwrap_or_else(|_| String::from("localhost:3001"));
 
     log::info!(target: "symfonia::api", "Starting HTTP Server");
     Server::new(TcpListener::bind(bind)).run(v9_api).await?;
     Ok(())
+}
+
+async fn custom_error(err: poem::Error) -> impl IntoResponse {
+    Json(json! ({
+        "message": err.to_string(),
+    }))
 }
