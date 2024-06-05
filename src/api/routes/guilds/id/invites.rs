eb8e55d55b9d3eac9895416b8ee9ead0f0738d4b
Quat3rnion <quat3rnion@proton.me>
2024-06-05 14:53:08 -0400

    Add skeleton for handling `GetInvitesSchema` query data

--- src/api/routes/guilds/id/invites.rs
+++ src/api/routes/guilds/id/invites.rs
@@ -1,7 +1,7 @@
+use chorus::types::{GetInvitesSchema, Snowflake};
 use chorus::types::jwt::Claims;
-use chorus::types::Snowflake;
 use poem::{handler, IntoResponse};
-use poem::web::{Data, Json, Path};
+use poem::web::{Data, Json, Path, Query};
 use sqlx::MySqlPool;
 
 use crate::database::entities::Guild;
@@ -12,6 +12,7 @@ pub async fn get_invites(
     Data(db): Data<&MySqlPool>,
     Data(claims): Data<&Claims>,
     Path(guild_id): Path<Snowflake>,
+    Query(query): Query<GetInvitesSchema>,
 ) -> poem::Result<impl IntoResponse> {
     let guild = Guild::get_by_id(db, guild_id)
         .await?
@@ -19,7 +20,14 @@ pub async fn get_invites(
 
     // TODO: Check if the user is allowed to see the invites of this guild
 
-    let invites = guild.get_invites(db).await?;
+    let mut invites = guild.get_invites(db).await?;
+
+    if query.with_counts.unwrap_or_default() {
+        // TODO: Get approximate member count
+        // TODO: Get approximate online member count (presences)
+
+        invites.iter_mut().for_each(|invite| {});
+    }
 
     Ok(Json(invites))
 }
