9fa798bc6e443aa432a9586b20c6ed2b837b7ce5
Quat3rnion <quat3rnion@proton.me>
2024-06-05 00:17:28 -0400

    Add modify and save methods to Channel entity

--- src/database/entities/channel.rs
+++ src/database/entities/channel.rs
@@ -1,8 +1,14 @@
-use crate::{database::Queryer, errors::Error};
-use chorus::types::{ChannelType, Snowflake};
+use std::ops::{Deref, DerefMut};
+
+use chorus::types::{
+    ChannelModifySchema, ChannelType, CreateChannelInviteSchema, InviteType, Snowflake,
+};
 use serde::{Deserialize, Serialize};
 use sqlx::MySqlPool;
-use std::ops::{Deref, DerefMut};
+use sqlx::types::Json;
+
+use crate::database::entities::invite::Invite;
+use crate::errors::Error;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, sqlx::FromRow)]
 pub struct Channel {
@@ -112,5 +118,63 @@ impl Channel {
             .await
             .map(|_| ())
             .map_err(Error::from)
+    }
+
+    pub fn modify(&mut self, data: ChannelModifySchema) {
+        self.name = data.name;
+        self.topic = data.topic;
+        self.nsfw = data.nsfw;
+        self.position = data.position;
+        self.permission_overwrites = data.permission_overwrites.map(Json);
+        self.rate_limit_per_user = data.rate_limit_per_user;
+        self.parent_id = data.parent_id;
+        self.bitrate = data.bitrate;
+        self.icon = data.icon;
+        self.user_limit = data.user_limit;
+        self.rtc_region = data.rtc_region;
+        self.default_auto_archive_duration = data.default_auto_archive_duration;
+        self.default_reaction_emoji = data.default_reaction_emoji.map(Json);
+        self.flags = data.flags;
+        self.default_thread_rate_limit_per_user = data.default_thread_rate_limit_per_user;
+        self.video_quality_mode = data.video_quality_mode;
+
+        if let Some(channel_type) = data.channel_type {
+            self.channel_type = channel_type;
+        }
+    }
+
+    pub async fn save(&self, db: &MySqlPool) -> Result<(), Error> {
+        sqlx::query("UPDATE channels SET name = ?, topic = ?, nsfw = ?, position = ?, permission_overwrites = ?, rate_limit_per_user = ?, parent_id = ?, bitrate = ?, icon = ?, user_limit = ?, rtc_region = ?, default_auto_archive_duration = ?, default_reaction_emoji = ?, flags = ?, default_thread_rate_limit_per_user = ?, video_quality_mode = ?, channel_type = ? WHERE id = ?")
+            .bind(&self.name)
+            .bind(&self.topic)
+            .bind(&self.nsfw)
+            .bind(&self.position)
+            .bind(&self.permission_overwrites)
+            .bind(&self.rate_limit_per_user)
+            .bind(&self.parent_id)
+            .bind(&self.bitrate)
+            .bind(&self.icon)
+            .bind(&self.user_limit)
+            .bind(&self.rtc_region)
+            .bind(&self.default_auto_archive_duration)
+            .bind(&self.default_reaction_emoji)
+            .bind(&self.flags)
+            .bind(&self.default_thread_rate_limit_per_user)
+            .bind(&self.video_quality_mode)
+            .bind(&self.channel_type)
+            .bind(&self.id)
+            .execute(db)
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn create_invite(
+        &self,
+        db: &MySqlPool,
+        payload: CreateChannelInviteSchema,
+        inviter_id: Option<Snowflake>,
+    ) -> Result<Invite, Error> {
+        Invite::create(db, payload, Some(self.id), inviter_id, InviteType::Guild).await
     }
 }
