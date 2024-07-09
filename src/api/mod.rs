f46abc2b1512bf0cf783b9f05cd149878f9e99b7
bitfl0wer <florian@pro-weber.com>
2024-07-09 22:42:28 +0200

    Move poem::Server starting logic

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,41 +1,15 @@
-use poem::{
-    EndpointExt,
-    IntoResponse,
-    listener::TcpListener,
-    middleware::{NormalizePath, TrailingSlash}, Route, Server, web::Json,
-};
-use serde_json::json;
-use sqlx::MySqlPool;
+use poem::{EndpointExt, Route};
 
-use crate::{
-    api::{
-        middleware::{
-            authentication::AuthenticationMiddleware, current_user::CurrentUserMiddleware,
-        },
-        routes::{auth, channels, guilds, users},
-    },
-    database::entities::Config,
-    errors::Error,
+use crate::api::{
+    middleware::{authentication::AuthenticationMiddleware, current_user::CurrentUserMiddleware},
+    routes::{auth, channels, guilds, users},
 };
+use crate::PathRouteTuple;
 
 mod middleware;
 mod routes;
 
-pub async fn start_api(db: MySqlPool) -> Result<(), Error> {
-    log::info!(target: "symfonia::api::cfg", "Loading configuration");
-    let config = Config::init(&db).await?;
-
-    if config.sentry.enabled {
-        let _guard = sentry::init((
-            "https://241c6fb08adb469da1bb82522b25c99f@sentry.quartzinc.space/3",
-            sentry::ClientOptions {
-                release: sentry::release_name!(),
-                traces_sample_rate: config.sentry.trace_sample_rate as f32,
-                ..Default::default()
-            },
-        ));
-    }
-
+pub fn setup_api() -> PathRouteTuple {
     let routes = Route::new()
         .nest("/auth", auth::setup_routes())
         .nest(
@@ -65,24 +39,5 @@ pub async fn start_api(db: MySqlPool) -> Result<(), Error> {
         .nest("/policies", routes::policies::setup_routes())
         .nest("/-", routes::health::setup_routes());
 
-    let v9_api = Route::new()
-        .nest("/api/v9", routes)
-        .data(db)
-        .data(config)
-        .with(NormalizePath::new(TrailingSlash::Trim))
-        .catch_all_error(custom_error);
-
-    let bind = std::env::var("API_BIND").unwrap_or_else(|_| String::from("localhost:3001"));
-
-    log::info!(target: "symfonia::api", "Starting HTTP Server");
-    Server::new(TcpListener::bind(bind)).run(v9_api).await?;
-    Ok(())
-}
-
-async fn custom_error(err: poem::Error) -> impl IntoResponse {
-    Json(json! ({
-        "success": false,
-        "message": err.to_string(),
-    }))
-    .with_status(err.status())
+    ("/api/v9".to_string(), routes)
 }
