144d24ade7a01ce998ddabe9a5f3b0d4c9834285
bitfl0wer <florian@pro-weber.com>
2024-07-25 11:14:06 +0200

    Add fn checked_add_new_connection, update other structs

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -41,28 +41,28 @@ Handling disconnects and session resumes is for later, I think.
 /// time.
 struct GatewayUser {
     /// Sessions a User is connected with.
-    clients: Vec<GatewayClient>,
+    pub clients: Vec<GatewayClient>,
     /// The Snowflake ID of the User.
-    id: Snowflake,
+    pub id: Snowflake,
     /// A collection of [Subscribers](Subscriber) to [Event] [Publishers](pubserve::Publisher).
     ///
     /// A GatewayUser may have many [GatewayClients](GatewayClient), but he only gets subscribed to
     /// all relevant [Publishers](pubserve::Publisher) *once* to save resources.
-    subscriptions: Vec<Box<dyn Subscriber<Event>>>,
+    pub subscriptions: Vec<Box<dyn Subscriber<Event>>>,
 }
 
 /// A concrete session, that a [GatewayUser] is connected to the Gateway with.
 struct GatewayClient {
     /// A [Weak] reference to the [GatewayUser] this client belongs to.
-    parent: Weak<GatewayUser>,
+    pub parent: Weak<Mutex<GatewayUser>>,
     /// The [TcpStream] for this WebSocket session
-    connection: TcpStream,
+    pub connection: TcpStream,
     /// [GatewayIdentifyPayload] the client has sent when connecting (or re-connecting) with this client.
-    identify: GatewayIdentifyPayload,
+    pub identify: GatewayIdentifyPayload,
 }
 
 struct NewConnection {
-    user: Option<GatewayUser>,
+    user: Arc<Mutex<GatewayUser>>,
     client: GatewayClient,
 }
 
@@ -75,15 +75,64 @@ pub async fn start_gateway(
     let try_socket = TcpListener::bind(&bind).await;
     let listener = try_socket.expect("Failed to bind to address");
 
-    let mut gateway_users: Arc<Mutex<BTreeMap<Snowflake, GatewayUser>>> =
+    let gateway_users: Arc<Mutex<BTreeMap<Snowflake, Arc<Mutex<GatewayUser>>>>> =
         Arc::new(Mutex::new(BTreeMap::new()));
-
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::task::spawn(establish_connection(stream));
+        match tokio::task::spawn(establish_connection(stream))
+            .await
+            .expect("The establish_connection task died") // QUESTION(bitfl0wer) Is it ok to .expect() here?
+        {
+            Ok(new_connection) => checked_add_new_connection(gateway_users.clone(), new_connection),
+            Err(_) => todo!(),
+        }
     }
     Ok(())
 }
 
+/// Handle the Gateway connection initalization process of a client connecting to the Gateway up
+/// until the point where we receive the identify payload, at which point a [NewConnection] will
+/// be returned.
+///
+/// If successful, returns a [NewConnection] with a new [Arc<Mutex<GatewayUser>>] and a
+/// [GatewayClient], whose `.parent` field contains a [Weak] reference to the new [GatewayUser].
 async fn establish_connection(stream: TcpStream) -> Result<NewConnection, Error> {
-    Ok(())
+    todo!()
+}
+
+/// Adds the contents of a [NewConnection] struct to a `gateway_users` map in a "checked" manner.
+///
+/// If the `NewConnection` contains a [GatewayUser] which is already in `gateway_users`, then
+/// change the `parent` of the `NewConnection` [GatewayClient] to the User
+/// from our `gateway_users` and push the client to the `clients` field of the User in our
+/// `gateway_users``.
+///
+/// Else, add the [new GatewayUser] and the new [GatewayClient] into `gateway_users` as-is.
+fn checked_add_new_connection(
+    gateway_users: Arc<Mutex<BTreeMap<Snowflake, Arc<Mutex<GatewayUser>>>>>,
+    new_connection: NewConnection,
+) {
+    // Make `new_connection` mutable
+    let mut new_connection = new_connection;
+    // To avoid having to get the lock a lot of times, lock once here and hold this lock for most
+    // of the way through this method
+    let new_connection_user = new_connection.user.lock().unwrap();
+    let mut locked_map = gateway_users.lock().unwrap();
+    // If our map contains the user from `new_connection` already, modify the `parent` of the `client`
+    // of `new_connection` to point to the user already in our map, then insert that `client` into
+    // the `clients` field of our existing user.
+    if locked_map.contains_key(&new_connection_user.id) {
+        let existing_user = locked_map.get(&new_connection_user.id).unwrap();
+        new_connection.client.parent = Arc::downgrade(existing_user);
+        existing_user
+            .lock()
+            .unwrap()
+            .clients
+            .push(new_connection.client);
+    } else {
+        // We cannot do `locked_map.insert(id, new_connection.user)` if new_connection is still
+        // locked. Just bind the id we need to a new variable, then drop the lock.
+        let id = new_connection_user.id;
+        drop(new_connection_user);
+        locked_map.insert(id, new_connection.user);
+    }
 }
