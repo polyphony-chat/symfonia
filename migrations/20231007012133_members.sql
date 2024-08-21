9e61afc84ad562bd7fcc1875af6d4172beab1e03
bitfl0wer <florian@pro-weber.com>
2024-08-22 00:36:15 +0200

    create sequence, reformat file

--- migrations/20231007012133_members.sql
+++ migrations/20231007012133_members.sql
@@ -1,23 +1,25 @@
+CREATE SEQUENCE members_index_seq;
+
 create table if not exists members
 (
-    index                        serial primary key,
-    id                           varchar(255) not null,
-    guild_id                     varchar(255) not null,
-    nick                         varchar(255) null,
-    joined_at                    timestamp     not null,
-    premium_since                bigint       null,
-    deaf                         smallint      not null,
-    mute                         smallint      not null,
-    pending                      smallint      not null,
-    settings                     text         not null,
-    last_message_id              varchar(255) null,
-    joined_by                    varchar(255) null,
-    avatar                       varchar(255) null,
-    banner                       varchar(255) null,
-    bio                          varchar(255) not null,
-    theme_colors                 text         null,
-    pronouns                     varchar(255) null,
-    communication_disabled_until timestamp     null,
+    index                        numeric(20, 0)     not null default nextval('user_settings_index_seq') constraint chk_index_range check (index >= 0 and index <= 18446744073709551615) primary key,
+    id                           varchar(255)       not null,
+    guild_id                     varchar(255)       not null,
+    nick                         varchar(255)       null,
+    joined_at                    timestamp          not null,
+    premium_since                bigint             null,
+    deaf                         smallint           not null,
+    mute                         smallint           not null,
+    pending                      smallint           not null,
+    settings                     text               not null,
+    last_message_id              varchar(255)       null,
+    joined_by                    varchar(255)       null,
+    avatar                       varchar(255)       null,
+    banner                       varchar(255)       null,
+    bio                          varchar(255)       not null,
+    theme_colors                 text               null,
+    pronouns                     varchar(255)       null,
+    communication_disabled_until timestamp          null,
     constraint IDX_bb2bf9386ac443afbbbf9f12d3
         unique (id, guild_id),
     constraint FK_16aceddd5b89825b8ed6029ad1c
@@ -27,3 +29,5 @@ create table if not exists members
         foreign key (id) references users (id)
             on delete cascade
 );
+
+ALTER SEQUENCE members_index_seq OWNED BY members.index;
