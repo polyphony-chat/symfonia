4364d49b16854a7da4df06c142606d142a570176
bitfl0wer <florian@pro-weber.com>
2024-08-22 00:44:03 +0200

    fix: multiple primary keys in member_roles migration

--- migrations/20231007012306_member_roles.sql
+++ migrations/20231007012306_member_roles.sql
@@ -2,7 +2,7 @@ CREATE SEQUENCE member_roles_index_seq;
 
 create table if not exists member_roles
 (
-    index   numeric(20, 0) not null default nextval('member_roles_index_seq') constraint chk_index_range check (index >= 0 and index <= 18446744073709551615) primary key,
+    index   numeric(20, 0) not null default nextval('member_roles_index_seq') constraint chk_index_range check (index >= 0 and index <= 18446744073709551615) unique,
     role_id varchar(255) not null,
     primary key (index, role_id),
     constraint FK_5d7ddc8a5f9c167f548625e772e
