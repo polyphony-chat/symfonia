d57d7c0f208a61138a68bbde75efc890043bc2f3
bitfl0wer <florian@pro-weber.com>
2024-09-25 20:20:56 +0200

    Lay out basics for gateway task

--- src/gateway/gateway_task.rs
+++ src/gateway/gateway_task.rs
@@ -2,6 +2,8 @@ use std::sync::Arc;
 
 use tokio::sync::Mutex;
 
+use crate::errors::Error;
+
 use super::{Event, GatewayClient};
 
 /// Handles all messages a client sends to the gateway post-handshake.
@@ -19,4 +21,26 @@ pub(super) async fn gateway_task(
 async fn handle_event(event: Event, connection: Arc<Mutex<super::WebSocketConnection>>) {
     // TODO
     todo!()
+}
+
+async fn process_inbox(
+    mut inbox: tokio::sync::broadcast::Receiver<Event>,
+    mut kill_receive: tokio::sync::broadcast::Receiver<()>,
+) -> Result<(), Error> {
+    tokio::select! {
+        _ = kill_receive.recv() => {
+            Ok(())
+        }
+        event = inbox.recv() => {
+            match event {
+                Ok(event) => {
+                    // TODO
+                    todo!()
+                }
+                Err(_) => {
+                    Ok(())
+                }
+            }
+        }
+    }
 }
