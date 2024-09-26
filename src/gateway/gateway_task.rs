8e95e49d0fb1054aeba059ea3490b62a375f9281
bitfl0wer <florian@pro-weber.com>
2024-09-26 21:32:47 +0200

    fn received_message_to_event(message: Message)

--- src/gateway/gateway_task.rs
+++ src/gateway/gateway_task.rs
@@ -1,12 +1,14 @@
 use std::sync::Arc;
 
-use chorus::types::GatewayHeartbeat;
+use chorus::types::{GatewayHeartbeat, GatewaySendPayload};
 use futures::StreamExt;
+use serde_json::from_str;
 use tokio::sync::Mutex;
+use tokio_tungstenite::tungstenite::Message;
 
 use crate::errors::Error;
 
-use super::{Event, GatewayClient};
+use super::{Event, GatewayClient, GatewayPayload};
 
 /// Handles all messages a client sends to the gateway post-handshake.
 pub(super) async fn gateway_task(
@@ -36,6 +38,29 @@ pub(super) async fn gateway_task(
     }
 
     todo!()
+}
+
+/// Convert a [Message] into an [Event], if the event message is a valid event that a server can
+/// expect to receive from a client.
+fn received_message_to_event(message: Message) -> Result<Event, Error> {
+    if !message.is_text() {
+        return Err(Error::Custom(
+            "Tungstenite message must be of type text".to_string(),
+        ));
+    }
+    let message_text = message.to_string();
+    let gateway_payload = from_str::<GatewayPayload<String>>(&message_text)?;
+    match gateway_payload.op_code {
+        1 => Ok(Event::Heartbeat(from_str(&message_text)?)),
+        2 => Ok(Event::Identify(from_str(&message_text)?)),
+        3 => Ok(Event::PresenceUpdate(from_str(&message_text)?)),
+        4 => Ok(Event::VoiceStateUpdate(from_str(&message_text)?)),
+        6 => Ok(Event::Resume(from_str(&message_text)?)),
+        8 => Ok(Event::GuildMembersRequest(from_str(&message_text)?)),
+        o => Err(Error::Custom(format!(
+            "opcode {o} is not a valid event to receive from a client"
+        ))),
+    }
 }
 
 async fn handle_event(event: Event, connection: super::WebSocketConnection) -> Result<(), Error> {
