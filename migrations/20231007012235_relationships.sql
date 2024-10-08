9131697a4e521555cb9e9b42c2e23dbec81b4285
bitfl0wer <florian@pro-weber.com>
2024-10-08 11:16:15 +0200

    Modify relationships table - Remove "id" in favor of index sequence - Change type of "type" from int to numeric(3, 0) - Add value constraints to type - Rename ORM-generated constraint names to human-readable names

--- migrations/20231007012235_relationships.sql
+++ migrations/20231007012235_relationships.sql
@@ -1,16 +1,18 @@
-create table if not exists relationships
+CREATE SEQUENCE relationships_index_seq;
+
+CREATE TABLE IF NOT EXISTS relationships
 (
-    id       numeric(20, 0) not null constraint chk_id_range check (id >= 0 AND id <= 18446744073709551615) primary key,
+    index    numeric(20, 0) not null default nextval('relationships_index_seq') constraint chk_index_range check (index >= 0 AND index <= 18446744073709551615) primary key,
     from_id  numeric(20, 0) not null constraint chk_from_id_range check (from_id >= 0 AND from_id <= 18446744073709551615),
     to_id    numeric(20, 0) not null constraint chk_to_id_range check (to_id >= 0 AND to_id <= 18446744073709551615),
     nickname varchar(255) null,
-    type     int          not null,
-    constraint IDX_a0b2ff0a598df0b0d055934a17
+    type     numeric(3, 0)  not null constraint chk_type_range check (type >= 0 AND type <= 255),
+    constraint FK_unique_relationships
         unique (from_id, to_id),
-    constraint FK_9af4194bab1250b1c584ae4f1d7
+    constraint FK_from_id_references_users_id
         foreign key (from_id) references users (id)
             on delete cascade,
-    constraint FK_9c7f6b98a9843b76dce1b0c878b
+    constraint FK_to_id_references_users_id
         foreign key (to_id) references users (id)
             on delete cascade
 );
