12bd3b95ee2b0f055bc103514f271d22a20e0062
bitfl0wer <florian@pro-weber.com>
2024-10-08 18:00:50 +0200

    add get_by_from_id

--- src/database/entities/relationship.rs
+++ src/database/entities/relationship.rs
@@ -1,17 +1,21 @@
 use std::ops::{Deref, DerefMut};
 
+use bigdecimal::BigDecimal;
 use chorus::types::{PublicUser, Snowflake};
 use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+
+use crate::errors::Error;
+use crate::QUERY_UPPER_LIMIT;
 
 use super::*;
 
 #[derive(sqlx::FromRow, Debug, Clone, Serialize, Deserialize)]
 pub struct Relationship {
     #[sqlx(flatten)]
-    #[serde(flatten)]
-    inner: chorus::types::Relationship,
+    pub(crate) inner: chorus::types::Relationship,
     pub from_id: Snowflake,
-    pub nickname: Option<String>,
+    #[sqlx(skip)]
     pub user: PublicUser,
 }
 
@@ -36,5 +40,19 @@ impl Relationship {
 
     pub fn as_inner(&self) -> &chorus::types::Relationship {
         &self.inner
+    }
+
+    pub fn as_inner_mut(&mut self) -> &mut chorus::types::Relationship {
+        &mut self.inner
+    }
+
+    // TODO: Write tests for this
+    pub async fn get_by_from_id(from_id: Snowflake, db: &PgPool) -> Result<Vec<Self>, Error> {
+        sqlx::query_as("SELECT * from relationships WHERE from_id = $1 LIMIT $2")
+            .bind(from_id)
+            .bind(QUERY_UPPER_LIMIT)
+            .fetch_all(db)
+            .await
+            .map_err(Error::from)
     }
 }
