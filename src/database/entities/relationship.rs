5e903bd006ed7d2601bdba0419e8b5beeaf171b8
bitfl0wer <florian@pro-weber.com>
2024-10-08 15:45:00 +0200

    Create basic relationship.rs

---
+++ src/database/entities/relationship.rs
@@ -0,0 +1,40 @@
+use std::ops::{Deref, DerefMut};
+
+use chorus::types::{PublicUser, Snowflake};
+use serde::{Deserialize, Serialize};
+
+use super::*;
+
+#[derive(sqlx::FromRow, Debug, Clone, Serialize, Deserialize)]
+pub struct Relationship {
+    #[sqlx(flatten)]
+    #[serde(flatten)]
+    inner: chorus::types::Relationship,
+    pub from_id: Snowflake,
+    pub nickname: Option<String>,
+    pub user: PublicUser,
+}
+
+impl Deref for Relationship {
+    type Target = chorus::types::Relationship;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl DerefMut for Relationship {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl Relationship {
+    pub fn to_inner(self) -> chorus::types::Relationship {
+        self.inner
+    }
+
+    pub fn as_inner(&self) -> &chorus::types::Relationship {
+        &self.inner
+    }
+}
