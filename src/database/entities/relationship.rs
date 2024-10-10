f5807f2f1b5494ad4126bb4e9851155b09f6b086
bitfl0wer <florian@pro-weber.com>
2024-10-10 23:40:02 +0200

    add get_by_to_id and get_all_by_id with tests

--- src/database/entities/relationship.rs
+++ src/database/entities/relationship.rs
@@ -56,6 +56,27 @@ impl Relationship {
             .await
             .map_err(Error::from)
     }
+
+    /// Retrieve all relationships where the specified user is the target
+    pub async fn get_by_to_id(to_id: Snowflake, db: &PgPool) -> Result<Vec<Self>, Error> {
+        sqlx::query_as("SELECT * from relationships WHERE to_id = $1 LIMIT $2")
+            .bind(to_id)
+            .bind(QUERY_UPPER_LIMIT)
+            .fetch_all(db)
+            .await
+            .map_err(Error::from)
+    }
+
+    /// Retrieve all relationships for a user by their ID, regardless of whether they are the source
+    /// or target of the relationship
+    pub async fn get_all_by_id(id: Snowflake, db: &PgPool) -> Result<Vec<Self>, Error> {
+        sqlx::query_as("SELECT * from relationships WHERE from_id = $1 OR to_id = $1 LIMIT $2")
+            .bind(id)
+            .bind(QUERY_UPPER_LIMIT)
+            .fetch_all(db)
+            .await
+            .map_err(Error::from)
+    }
 }
 
 #[cfg(test)]
@@ -88,5 +109,77 @@ mod relationship_unit_tests {
         assert_eq!(relationship.id, 7248639891561517057.into());
         assert_eq!(relationship.nickname, Some("Janana Banana 🍌".to_string()));
         assert_eq!(relationship.relationship_type, RelationshipType::Outgoing);
+    }
+
+    #[sqlx::test(fixtures(path = "../../../fixtures", scripts("users")))]
+    async fn get_by_to_id(pool: PgPool) {
+        sqlx::query!(
+            "INSERT INTO relationships(from_id, to_id, nickname, type) VALUES($1, $2, $3, $4);",
+            BigDecimal::from(7248639845155737600_u64),
+            BigDecimal::from(7248639891561517057_u64),
+            "Janana Banana 🍌",
+            BigDecimal::from(RelationshipType::Outgoing as u8)
+        )
+        .execute(&pool)
+        .await
+        .unwrap();
+
+        let relationships = super::Relationship::get_by_to_id(7248639891561517057.into(), &pool)
+            .await
+            .unwrap();
+
+        assert_eq!(relationships.len(), 1);
+        let relationship = &relationships[0];
+        assert_eq!(relationship.from_id, 7248639845155737600.into());
+        assert_eq!(relationship.id, 7248639891561517057.into());
+        assert_eq!(relationship.nickname, Some("Janana Banana 🍌".to_string()));
+        assert_eq!(relationship.relationship_type, RelationshipType::Outgoing);
+    }
+
+    #[sqlx::test(fixtures(path = "../../../fixtures", scripts("users")))]
+    async fn get_all_by_id(pool: PgPool) {
+        sqlx::query!(
+            "INSERT INTO relationships(from_id, to_id, nickname, type) VALUES($1, $2, $3, $4);",
+            BigDecimal::from(7248639845155737600_u64),
+            BigDecimal::from(7248639891561517057_u64),
+            "Janana Banana 🍌",
+            BigDecimal::from(RelationshipType::Outgoing as u8)
+        )
+        .execute(&pool)
+        .await
+        .unwrap();
+
+        let relationships = super::Relationship::get_all_by_id(7248639845155737600.into(), &pool)
+            .await
+            .unwrap();
+
+        assert_eq!(relationships.len(), 1);
+        let relationship = &relationships[0];
+        assert_eq!(relationship.from_id, 7248639845155737600.into());
+        assert_eq!(relationship.id, 7248639891561517057.into());
+        assert_eq!(relationship.nickname, Some("Janana Banana 🍌".to_string()));
+        assert_eq!(relationship.relationship_type, RelationshipType::Outgoing);
+
+        sqlx::query!(
+            "INSERT INTO relationships(from_id, to_id, nickname, type) VALUES($1, $2, $3, $4);",
+            BigDecimal::from(7248639845155737600_u64),
+            BigDecimal::from(7248640296244744192_u64),
+            "Banana Janana 🍌",
+            BigDecimal::from(RelationshipType::Incoming as u8)
+        )
+        .execute(&pool)
+        .await
+        .unwrap();
+
+        let relationships = super::Relationship::get_all_by_id(7248639845155737600.into(), &pool)
+            .await
+            .unwrap();
+
+        assert_eq!(relationships.len(), 2);
+        let relationship = &relationships[1];
+        assert_eq!(relationship.from_id, 7248639845155737600.into());
+        assert_eq!(relationship.id, 7248640296244744192.into());
+        assert_eq!(relationship.nickname, Some("Banana Janana 🍌".to_string()));
+        assert_eq!(relationship.relationship_type, RelationshipType::Incoming);
     }
 }
