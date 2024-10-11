d18cad179cddd1d5e61dc7b8f38444d76e0e36e4
bitfl0wer <florian@pro-weber.com>
2024-10-12 00:14:35 +0200

    Always run sqlx prepare pre-commit

--- prepare-sqlx-if-needed.sh
+++ prepare-sqlx-if-needed.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-if git diff --quiet HEAD -- "./migrations"; then
-    echo "No changes in migrations folder"
-    exit 0
-else
-    echo "Changes in migrations folder detected, running sqlx prepare"
-    cargo sqlx prepare --all -- --all-targets --all-features
-    git add .sqlx/*
-    exit_code=$?
+cargo sqlx prepare --all -- --all-targets --all-features
+git add .sqlx/*
+exit_code=$?
 
-    exit $exit_code
-fi
+exit $exit_code
