d17e1ceb5f02690b51401ebafbbe877066da5b85
Quaternion <quaternion@lumina.software>
2024-10-27 01:24:51 -0400

    Fill in info for the ping route

--- src/api/routes/ping.rs
+++ src/api/routes/ping.rs
@@ -2,26 +2,29 @@ use std::env;
 
 use chorus::types::{PingInstance, PingReturn};
 use poem::{handler, web::Json, IntoResponse, Route};
+use poem::web::Data;
 use serde::Serialize;
+use crate::database::entities::Config;
 
 pub fn setup_routes() -> Route {
     Route::new().at("/ping", ping)
 }
 
 #[handler]
-pub async fn ping() -> poem::Result<impl IntoResponse> {
+pub async fn ping(
+    Data(config): Data<&Config>
+) -> poem::Result<impl IntoResponse> {
     let ping_response = PingReturn {
-        // TODO: Fill this with configuration values
         ping: "pong!".to_string(),
         instance: PingInstance {
-            id: Some(1.into()),
-            name: "todo".to_string(),
-            description: None,
-            image: None,
-            correspondence_email: None,
-            correspondence_user_id: None,
-            front_page: None,
-            tos_page: None,
+            id: config.general.instance_id,
+            name: config.general.instance_name.to_owned(),
+            description: config.general.instance_description.to_owned(),
+            image: config.general.image.to_owned(),
+            correspondence_email: config.general.correspondence_email.to_owned(),
+            correspondence_user_id: config.general.correspondence_user_id.to_owned(),
+            front_page: config.general.front_page.to_owned(),
+            tos_page: config.general.tos_page.to_owned(),
         },
     };
     Ok(Json(ping_response))
