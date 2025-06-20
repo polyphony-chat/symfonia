390e56cad0c23414ef0267d6c83fd30f1c942282
bitfl0wer <florian@pro-weber.com>
2025-06-20 12:57:35 +0200

    feat: reorganize migrations, start creating missing tables

--- crates/util/migrations/00006_message_data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS message_data (
-    id BIGSERIAL PRIMARY KEY,
-    content TEXT NULL,
-    -- TODO: Make these proper types/fk's
-    attachments TEXT [] NULL,
-    embeds TEXT [] NULL,
-    reactions TEXT [] NULL
-);
--- crates/util/migrations/00007_messages.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (
-    version numeric(20, 0) NOT NULL CONSTRAINT chk_range CHECK (version >= 0 AND version <= 18446744073709551615),
-    uid uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-    author text NOT NULL,
-    timestamp timestamp
-);
